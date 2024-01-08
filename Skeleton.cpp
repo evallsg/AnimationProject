a56fc3b46030f83272011da0ce1fac2303fc7762
evallsg <evallsg@gmail.com>
2024-01-09 00:35:37 +0100

    skeleton implemented

---
+++ Skeleton.cpp
@@ -0,0 +1,40 @@
+#include "Skeleton.h"
+Skeleton::Skeleton() { }
+Skeleton::Skeleton(const Pose& rest, const Pose& bind, const std::vector<std::string>& names) {
+	Set(rest, bind, names);
+}
+
+void Skeleton::Set(const Pose& rest, const Pose& bind,
+	const std::vector<std::string>& names) {
+	mRestPose = rest;
+	mBindPose = bind;
+	mJointNames = names;
+	UpdateInverseBindPose();
+}
+//Any time the bind pose of the skeleton is updated, the inverse bind pose should be re-calculated as well.
+void Skeleton::UpdateInverseBindPose() {
+	unsigned int size = mBindPose.Size();
+	mInvBindPose.resize(size);
+	for (unsigned int i = 0; i < size; ++i) {
+		//Get the world space transform of each joint, convert it into a matrix, and invert the matrix
+		Transform world = mBindPose.GetGlobalTransform(i);
+		mInvBindPose[i] = inverse(transformToMat4(world));
+	}
+}
+
+//getters and setters
+Pose& Skeleton::GetBindPose() {
+	return mBindPose;
+}
+Pose& Skeleton::GetRestPose() {
+	return mRestPose;
+}
+std::vector<mat4>& Skeleton::GetInvBindPose() {
+	return mInvBindPose;
+}
+std::vector<std::string>& Skeleton::GetJointNames() {
+	return mJointNames;
+}
+std::string& Skeleton::GetJointName(unsigned int idx) {
+	return mJointNames[idx];
+}
---
+++ Skeleton.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <vector>
+#include "Pose.h"
+class Skeleton
+{
+protected:
+	Pose mRestPose;
+	Pose mBindPose;
+	std::vector<mat4> mInvBindPose;
+	std::vector<std::string> mJointNames;
+
+protected:
+	//updates the inverse bind pose matrix any time that the bind pose is set
+	void UpdateInverseBindPose();
+
+public:
+	Skeleton();
+	Skeleton(const Pose& rest, const Pose& bind, const std::vector<std::string>& names);
+
+	void Set(const Pose& rest, const Pose& bind, const std::vector<std::string>& names);
+
+	Pose& GetBindPose();
+	Pose& GetRestPose();
+	std::vector<mat4>& GetInvBindPose();
+	std::vector<std::string>& GetJointNames();
+	std::string& GetJointName(unsigned int index);
+}; // End Skeleton class
+};
+
